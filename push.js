8759965b8f2bc8e5fd154d8d6b90705786944df2
Anita <anita@timedoor.net>
2020-11-19 05:47:48 +0800

    fix payload notif

--- push.js
+++ push.js
@@ -1,21 +1,21 @@
 const webPush = require('web-push');
  
 const vapidKeys = {
-   "publicKey": "BBqAjDv59YRGw0qdRkpw2SiVaUFbA8FGCmiNejhUqwUQBnkwyaKaHig_IF6Qy5b0Sswjo6Spyr8IevLrV1tNoug",
-   "privateKey": "l8wc-3ivDwu5wBaOzbMDJrPVr_AHb2PenuHJg6Zrb_Q"
+   "publicKey": "BPo2DFQk0ndeUuyc-oMtGd4FbejHKU82dPSQ5O_hGXiRxMtG6kTYP7JZGrnbcu71fPnh_QXnMz0LPgXBoAnp3WE",
+   "privateKey": "QBg40mzQ2_vJVio-1rIsuTO-XZW_NSEKgfCIqgKpLDY"
 };
  
  
 webPush.setVapidDetails(
-   'mailto:example@yourdomain.org',
+   'mailto:anitadewi7789@gmail.com',
    vapidKeys.publicKey,
    vapidKeys.privateKey
 )
 const pushSubscription = {
-   "endpoint": "https://fcm.googleapis.com/fcm/send/eZ4HwUOJmoI:APA91bF4tA9jgAfkAQiwhqXBdmGH4d-IiwfRJEuPfQsP_TJKS9dm0fXRPzSE47-xZhZIEMojy9yrok_l_UOV18myXBADcaDwnEUIWAULIxjIQI8Jw_cnQaXeQtIBlC5rWS8Lb_xkpZbn",
+   "endpoint": "https://fcm.googleapis.com/fcm/send/e2BWfjn_LR0:APA91bGLBWWXZ90rcF5p1YUqyIFuR94ouYsdp5msLpWoC_qJQAi4mJrfMEBOc-u8S6VWoPAUxX_zqr2Q4IOo9RKXXXZ9R5jqaq98-Ndm9xRKH88tF8zsyhrHpT5D8onWkkZ0uHOVK4oS",
    "keys": {
-       "p256dh": "BO8CrhahesmfHZT4Xpt1tSW/ki7BRCtQxF1EEROJTj/cTYghJ94WLxphkvXQZrXt/WbmvhMFla5a0zAuEzZds3A=",
-       "auth": "/4mriBurxcz5tZ6vWjRdug=="
+       "p256dh": "BHwD1Ebh3buGcvP9LbeAeP44O5eJrOW/c0QkcILELg+mjNV9Ao2qxguixeMjVOJTRtwMuFik8VysRMR44J9zr3k=",
+       "auth": "lmTkPITRImfOSnJTioGRlQ=="
    }
 };
 const payload = 'Yay ! Now you can receive push notification!';
